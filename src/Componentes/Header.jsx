9e64a6c8cb786354c1d19617059268f3514eb9f2
Zubain-Nadeem <Zubainali23@gmail.com>
2025-06-25 07:50:06 -0700

    Header compelete

---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header className=" h-16 bg-[#FFFFFF] flex justify-center items-center w-[100%] fixed z-50 shadow-lg">
+        <div className=" h-full w-[85%] flex items-center justify-between">
+          <h1 className="text-2xl font">
+            My <span className="text-[#006FF0]">profile picture</span>
+          </h1>
+
+          <nav>
+            <ul className="flex gap-8 text-sm text-slate-700">
+              <li>
+                <a href="#">For companies</a>
+              </li>
+              <li>
+                <a href="#">How it works</a>
+              </li>
+              <li>
+                <a href="#">Price</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Examples</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className=" flex gap-3 ">
+            <button className="px-5 py-3 border border-slate-600 font-semibold rounded-2xl cursor-pointer">
+              Register
+            </button>
+            <button className="px-5 py-3.5 text-white rounded-2xl bg-orange-400 cursor-pointer hover:bg-slate-600">
+              Start now
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
