5359415a21088414c7b4192e9156c3d71162d0f7
Zubain-Nadeem <Zubainali23@gmail.com>
2025-06-27 23:04:50 -0700

    Cards Created

--- src/Componentes/ThreeStep.jsx
+++ src/Componentes/ThreeStep.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const ThreeStep = () => {
   return (
-    <div>
+    <div className='border-2 border-yellow-600 min-h-[400px] bg-[#F3F2EF]'>
+        <div className='border border-black mx-auto w-[85%] py-6'>
+            <h1 className='text-6xl text-center mb-6'>It's that simple: 3 steps to your professional application photo</h1>
+            <div className='border border-red-500 flex justify-between'>
+                <div className='border border-indigo-400 w-[30%] h-[350px] bg-white rounded-2xl '>
+                    
+                </div>
+                 <div className='border border-indigo-400 w-[30%] h-[350px] bg-white rounded-2xl '>
+                    
+                </div>
+                 <div className='border border-indigo-400 w-[30%] h-[350px] bg-white rounded-2xl '>
+                    
+                </div>
+            </div>
+        </div>
       
     </div>
   )
